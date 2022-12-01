0c40fee2881c5e523948378dcfcfaa4f5ae411c6
jej1217s1217 <jej1217s1217@gmail.com>
2022-12-01 09:27:46 +0900

    jej vages, tubular

---
+++ javascript/jejvegas.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function () {
+   // vages
+   $('#main_visual').vegas({
+      slides: [
+         {src: '../resources/images/jungenjin/main.jpg'},
+         {src: '../resources/images/jungenjin/main2.jpg'},
+         {src: '../resources/images/jungenjin/main3.jpg'},
+         {src: '../resources/images/jungenjin/main4.jpg'},
+      ],
+   });
+});
---
+++ javascript/jungenjin.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+   //tublur
+   $('document').ready(function () {
+      var options = {videoId: 'uWlCbRhYT7Y', mute: true};
+      $('header, #main_visual, section').tubular(options);
+   });
+});
